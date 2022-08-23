a9f4b37955df51c1c45fef0300ee805a04cc32a6
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-08-23 11:56:01 +0530

    Build and deploy website as part of GitHub action

--- scripts/deploy-site.sh
+++ scripts/deploy-site.sh
@@ -3,18 +3,18 @@
 HERE=$(dirname $0)
 
 pushd "${HERE}/../frontend"
-GIT_URL=$(git remote get-url origin)
-
 npm ci
 npm run build
 
 pushd build
 git init
+cp ../../.git/config .git/config
+git branch -m gh-pages
 git add .
 git config user.email "noreply@indiaultimate.org"
 git config user.name "India Ultimate GitHub Bot"
 git commit -m "New website build"
-git push --force "${GIT_URL}" main:gh-pages
+git push --force origin gh-pages:gh-pages
 popd
 
 popd
