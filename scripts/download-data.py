fb231a14b3bf9c8978dd2fdbb2bbdb96abf3a916
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-08-23 09:21:45 +0530

    Add code to download stats for specific teams from Ulti Analytics

--- scripts/download-data.py
+++ scripts/download-data.py
@@ -1,3 +1,4 @@
+import cgi
 import os
 from pathlib import Path
 
@@ -16,9 +17,25 @@ def get_fantasy_teams_csv(sheet_id):
     return path
 
 
+def get_stats_csvs(team_ids):
+    for team_id in team_ids:
+        url = f"https://www.ultianalytics.com/rest/view/team/{team_id}/stats/export"
+        response = requests.get(url)
+        header = response.headers.get("Content-Disposition", "")
+        _, headers = cgi.parse_header(header)
+        filename = headers.get("filename", "")
+        name, ext = os.path.splitext(filename)
+        filename = f"{name}-{team_id}{ext}"
+        with open(DATA_DIR.joinpath(filename), "w") as f:
+            f.write(response.text)
+
+
 def main():
     sheet_id = os.environ["GOOGLE_SHEET_ID"]
     get_fantasy_teams_csv(sheet_id)
+
+    team_ids = os.environ["ULTI_ANALYTICS_TEAM_IDS"].split()
+    get_stats_csvs(team_ids)
 
 
 if __name__ == "__main__":
