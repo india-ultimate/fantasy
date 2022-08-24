2e6d4b55da1a87e034050536749646a02af0dece
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-08-24 16:39:32 +0530

    WIP Add teams list

---
+++ frontend/src/components/TeamList.jsx
@@ -0,0 +1,19 @@
+const teams = [
+	{
+		name: "Karnataka Squirrels",
+		logo:
+			"https://res.cloudinary.com/india-ultimate/image/upload/v1661335497/ultimate-fantasy-league/sponsor-logos.png",
+		iuSlug: "delhi-ultimate",
+	},
+	{
+		name: "Karnataka Squirrels",
+		logo:
+			"https://res.cloudinary.com/india-ultimate/image/upload/v1661335497/ultimate-fantasy-league/sponsor-logos.png",
+		iuSlug: "delhi-ultimate",
+	},
+];
+const TeamList = () => {
+	return <p>Yo!</p>;
+};
+
+export default TeamList;
