b234383facdd594e6ba713dd746aa2a353d1f209
Puneeth Chaganti <punchagan@muse-amuse.in>
2022-08-27 23:09:50 +0530

    Fix broken link to player profile in PlayerRow

--- frontend/src/components/PlayerRow.jsx
+++ frontend/src/components/PlayerRow.jsx
@@ -1,7 +1,9 @@
 import { Avatar, Link, TableCell, TableRow, Typography } from "@mui/material";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const PlayerRow = ({ player }) => {
+	const navigate = useNavigate();
 	return (
 		<TableRow
 			key={player.ucID}
